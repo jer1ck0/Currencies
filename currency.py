56bb2e8e587707151dee5d3da4256b1775451a1b
my first twink <jer1ck0@lex.lebedev.example.com>
2021-07-08 22:33:17 +0300

    Move global to class var, replace date to datetime, split params in func, iso date format

--- currency.py
+++ currency.py
@@ -1,40 +1,33 @@
 import datetime
-global rates
-rates = {"USD": [(datetime.date(2021, 5, 1), 71), (datetime.date(2021, 5, 2), 72), (datetime.date(2021, 5, 3), 73), (datetime.date(2021, 5, 4), 74), (datetime.date(2021, 5, 5), 75)],
-         "RUB": [(datetime.date(2021, 5, 1), 1), (datetime.date(2021, 5, 2), 2), (datetime.date(2021, 5, 3), 3), (datetime.date(2021, 5, 4), 4), (datetime.date(2021, 5, 5), 5)]}
 
 class Rate():
+    rates = {"USD": [(datetime.datetime(2021, 5, 1), 71), (datetime.datetime(2021, 5, 2), 72), (datetime.datetime(2021, 5, 3), 73), (datetime.datetime(2021, 5, 4), 74), (datetime.datetime(2021, 5, 5), 75)],
+         "RUB": [(datetime.datetime(2021, 5, 1), 1), (datetime.datetime(2021, 5, 2), 2), (datetime.datetime(2021, 5, 3), 3), (datetime.datetime(2021, 5, 4), 4), (datetime.datetime(2021, 5, 5), 5)]}
 
-    def add(request):
-        currency = request[0]
-        rate = request[1]
-        date = datetime.date(request[2], request[3], request[4])
-        print (currency, rate, date)
-        print (rates[currency])
-        print (date not in [element[0] for element in rates[currency]])
-        if date not in [element[0] for element in rates[currency]]:
-            rates[currency].append((date, rate))
-            rates[currency].sort()
+    def add(currency, rate, date):
+        print(type(date))
+        if date not in [element[0] for element in Rate.rates[currency]]:
+            Rate.rates[currency].append((date, int(rate)))
+            Rate.rates[currency].sort()
             return "Succesfull added rate"
         else:
             return "Rate already exist"
 
-    def find(currency, date = datetime.date.today()):
+    def find(currency, date):
         start_index = 0
-        end_index = len(rates[currency]) - 1
+        end_index = len(Rate.rates[currency]) - 1
         while True:
             index = int(((start_index + end_index)/2)//1)
-            if rates[currency][index][0] == date:
-                return rates[currency][index][1]
-            elif rates[currency][index][0] < date:
+            if Rate.rates[currency][index][0] == date:
+                return Rate.rates[currency][index][1]
+            elif Rate.rates[currency][index][0] < date:
                 start_index = index + 1
             else:
                 end_index = index - 1
             if start_index > end_index:
-                return rates[currency][index][1]
+                return Rate.rates[currency][index][1]
     
-    def compare(request):
-        date = datetime.date(request[2], request[3], request[4])
-        return Rate.find(request[0], date)/Rate.find(request[1], date)
+    def compare(currency1, currency2, date):
+        return Rate.find(currency1, date)/Rate.find(currency2, date)
 
             
--- server.py
+++ server.py
@@ -1,37 +1,35 @@
-from datetime import date
-from currency import Rate, rates
+import datetime
+from currency import Rate
 from flask import Flask, json, request
 
 app = Flask(__name__)
 
 @app.route("/base")
 def base_info():
-    return rates
+    return Rate.rates
 
 
 @app.route("/compare")
 def comparing():
     currency1 = request.args.get('1c')
     currency2 = request.args.get('2c')
-    day, month, year = request.args.get('date').split('-') if request.args.get('date') != None else [date.today().day, date.today().month, date.today().year]
-    target_request = (currency1, currency2, int(year), int(month), int(day))
+    date = datetime.datetime.now() if request.args.get('date') == None else datetime.datetime.strptime(request.args.get('date'), "%Y-%m-%dT%H:%M:%S")
     if currency1 == None or currency2 == None:
         return "Wrong format of request"
     else:
-        target = Rate.compare(target_request)
+        target = Rate.compare(currency1, currency2, date)
         return str(target)
       
 @app.route("/add")
 def adding():
     currency = request.args.get('cur')
     rate = request.args.get('rate')
-    day, month, year = request.args.get('date').split('-') if request.args.get('date') != None else [date.today().day, date.today().month, date.today().year]
-    target_request = (currency, rate, int(year), int(month), int(day))
+    date = datetime.datetime.now() if request.args.get('date') == None else datetime.datetime.strptime(request.args.get('date'), "%Y-%m-%dT%H:%M:%S")
     if currency == None or rate == None:
         return "Wrong format of adding request"
     else:
-        Rate.add(target_request)
-        return f"Added rate{rates}"
+        Rate.add(currency, rate, date)
+        return Rate.rates
 
 if __name__ == '__main__':
     app.run(host='45.143.136.88', port='5000', debug=True)
